4456bf561e6e7e481c58a9fc7dff9736cca59d30
bill <bill@bill-M750SLI-DS4>
2012-07-06 15:07:14 -0700

    added a method to conver co-occurrence counts to tfidf sparse matrix

--- src/data_preprocess/util.py
+++ src/data_preprocess/util.py
@@ -1,6 +1,12 @@
 import numpy as np
 import scipy as sp
 from scipy.sparse import *
+from gensim import models, matutils
+
+import sys
+sys.path.append('../')
+from similarity import *
+from data_preprocess import *
 
 def counts_to_probs(count_mat):
     '''
@@ -25,3 +31,18 @@ def prune_probs(probs):
 def prune_by_MI():
     'prune by mutual information'
     pass
+
+def tfidf(counts_mat):
+    m,n = counts_mat.shape
+    # utilize gensim
+    corpus = matutils.Sparse2Corpus(counts_mat)
+    tfidf = models.logentropy_model.LogEntropyModel(corpus)
+    c_tfidf = tfidf[corpus]
+    return matutils.corpus2csc(c_tfidf)
+    
+if __name__ == "__main__":
+    NO = pickle.load(open('/var/wordsim/preprocessed_7.2.12/ngram_occurs.p','rb'))
+    occurs = NO.get_occurs(concat=True)
+    print occurs
+    m = tfidf(occurs)
+    print m
