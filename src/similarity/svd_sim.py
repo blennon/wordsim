95d3475135be9f77b39f89a15337a674c8ddc894
bill <bill@bill-M750SLI-DS4>
2012-07-31 16:48:49 -0700

    fixed to perform svd on probs mat not count mat, tested a different sparse svd package but got similar results to built in scipy package

--- src/similarity/svd_sim.py
+++ src/similarity/svd_sim.py
@@ -7,6 +7,7 @@ import sys
 sys.path.append('../')
 import numpy as np
 from scipy.sparse.linalg import svds
+#from sparsesvd import sparsesvd as svds
 import cPickle as pickle
 from data_preprocess import *
 sys.modules['lexicon'] = lexicon
@@ -14,14 +15,15 @@ from similarity import *
 
 class SVDSim(Similarity):
 
-    def transform_data(self, occurs, d):
+    def transform_data(self, occurs, d, min_prob=1e-3):
         '''
         perform sparse svd on the data with d SVs
         
         return data transformed into similarity space
         '''
         probs = counts_to_probs(occurs)
-        P,D,Qt = svds(occurs,d)
+        probs = prune_by_min(probs,min_prob)
+        P,D,Qt = svds(probs,d)
         if self.norm_ss:
             return SVDSim._norm_mat(P*D)
         else:
